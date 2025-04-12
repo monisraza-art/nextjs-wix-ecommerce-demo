d1ea56da7a1487bc7bec8d2170a4622634d6e020
Monis <monisraza444@gmail.com>
2025-04-12 16:53:42 +0500

    Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,16 @@ export default function Home() {
   return (
     <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
       <div className="flex items-center bg-secondary md:h-96">
-        <div className="space-y-7 p-10 text-center md:w-1/2">
-          <h1 className="text-3xl font-bold md:text-4xl">
-            Fill the void in your heart
+        <div className="space-y-7 p-10 text-center   md:w-1/2">
+          <h1 className="text-xl lg:text-4xl ">
+          This is a demo version of a fully customizable eCommerce store built with Next.js and Wix Studio.
           </h1>
           <p>
-            Tough day? Credit card maxed out? Buy some expensive stuff and
-            become happy again!
+          It showcases the design, layout, and dynamic features like product listings, filters, and more. 
+          Checkout and login functionalities can be integrated upon request.
+          </p>
+          <p className="font-bold">
+          Created by : Monis Raza 
           </p>
           <Button asChild>
             <Link href="/shop">
